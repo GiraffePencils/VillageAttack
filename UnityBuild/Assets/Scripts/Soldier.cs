99b31332aeacde1839127aab7ce0f7f71a18d972
GiraffePencils <giraffes_eat_pencils@hotmail.co.uk>
2015-01-26 22:40:11 +0000

    Beginings
    Setup the scene, basic Character soldier moving

---
+++ UnityBuild/Assets/Scripts/Soldier.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Soldier : MonoBehaviour {
+    CharacterController m_characterController;
+    int m_speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_speed = 1;
+        m_characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        m_characterController.Move(transform.forward * m_speed * Time.deltaTime);
+	}
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.CompareTag("Wall"))
+        {
+            m_speed = 0;
+        }
+
+    }
+    
+}
